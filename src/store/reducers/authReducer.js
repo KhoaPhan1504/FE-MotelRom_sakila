9d344aec44f238d4edcdb2246f6cfbc19e1bd51e
KhoaPhan1504 <Khoahttt01@gmail.com>
2024-06-14 16:23:58 +0700

    Initial project, tailwindcss, redux, redux-persist

---
+++ src/store/reducers/authReducer.js
@@ -0,0 +1,14 @@
+const initState = {
+    isLoggedIn: false,
+    token: 123222
+}
+
+const authReducer = (state = initState, action) => {
+    switch (action.type) {
+        
+        default:
+            return state;
+    }
+}
+
+export default authReducer;
